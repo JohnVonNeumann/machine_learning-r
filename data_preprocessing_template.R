c6329cd19841a89b36ed870f03eafb5f5f3191d1
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-24 18:02:23 +1100

    syntax was as i had hoped, all very similar except for obvious alterations of what it is that you are calling the methods on. an observation that ive had is that R is very syntax tight, capitalisation is important, you will get strange results with badly written, tended for code. this finishes me up for r preprocessing, r is way cleaner than python, like, way cleaner. welcome change in comparison to shit like js

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -5,6 +5,12 @@
 dataset = read.csv('Data.csv')
 
 # Taking care of missing data
+# Cleaning Age column
 dataset$Age = ifelse(is.na(dataset$Age), # using dataset var, we target the Age column and cells that are N/A
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), # we wish to average all N/A cells with a inbuilt mean function
                      dataset$Age) # we apply the changes to the Age column 
+
+# Cleaning Salary column
+dataset$Salary = ifelse(is.na(dataset$Salary), #same process as last block, just pertaining to Salary changes
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
