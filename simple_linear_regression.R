a7c244670f54a701aed9418bf64913a439f02cb8
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-28 21:03:17 +1100

    my attempt at preprocesing the salary_data set for our r implementation, i havent scaled as we didnt need to in python and im assuming that seeing as r is so data friendly, parts of what we are due to call will autoscale for us. added commments to reference my thoughts, next commit will be the instructors version/edits to my code with his recommendations

---
+++ simple_linear_regression.R
@@ -0,0 +1,13 @@
+# Simple Linear Regression
+
+# Import the datasets
+dataset = read.csv('Salary_Data.csv') # due to only having two columns, no need to select columns
+
+#Splitting dataset for use in testing and training
+library(caTools)
+set.seed(123)
+split = sample.split( dataset$YearsExperience, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE) # split = true pertains to the 2/3 figure we param'd above
+test_set = subset(dataset, split == FALSE) # split = false indicates we wish to allocate the remaining 1/3 of data to test
+
+#I'm going to hazard a guess that we won't need to scale and that R takes care of it for us.
