d494dbaf2c3919d9854978a85173a6af5720534a
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-28 21:11:30 +1100

    the only change to be made was where i split the data and split it wrong, instead of choosing the dep var, i chose the indep var, i think the reason we choose the dep var is because that is the one we are trying to find more information on, i cant think of another reason and to be fair im wondering if it would make any difference seeing as it obviously is splitting the yearsexp into that along with it, so perhaps this split makes no difference, i will ask the question in forums

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -6,7 +6,7 @@ dataset = read.csv('Salary_Data.csv') # due to only having two columns, no need 
 #Splitting dataset for use in testing and training
 library(caTools)
 set.seed(123)
-split = sample.split( dataset$YearsExperience, SplitRatio = 2/3)
+split = sample.split( dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE) # split = true pertains to the 2/3 figure we param'd above
 test_set = subset(dataset, split == FALSE) # split = false indicates we wish to allocate the remaining 1/3 of data to test
 
