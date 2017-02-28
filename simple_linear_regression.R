7f74ab4c2eb19a566d0bc74a6aa61c609c70af44
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-28 22:14:42 +1100

    my attempt at fitting the simple linear regression to the training set, i didnt get it right but used an online guide where i think i got some aspects right except because i saw him write the first object, im kinda experiencing some einstellung of the problem, cant see past what he wrote and ive hemmed my answer into fit his, next commit will be a fixup

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -10,4 +10,11 @@ split = sample.split( dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE) # split = true pertains to the 2/3 figure we param'd above
 test_set = subset(dataset, split == FALSE) # split = false indicates we wish to allocate the remaining 1/3 of data to test
 
-#I'm going to hazard a guess that we won't need to scale and that R takes care of it for us.
+#The library we are using takes care of scaling for us
+
+#Fitting Simple Linear Regression to the Training Set
+regressor(Salary ~ YearsExperience, data = training_set,
+          xlab = "Years Experience",
+          ylab = "Salary",
+          main = "Years Experience vs Salary (Training Set)"
+          )
